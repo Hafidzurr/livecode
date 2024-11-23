e31cbdc83a1eede3b1a9cd36d564e7040d22ce8a
Hafidzurrohman Saifullah <hafidzurrohmansaifullah@gmail.com>
2024-11-23 12:39:24 +0700

    soal1

---
+++ soal1.js
@@ -0,0 +1,8 @@
+let nama = "hafidz";
+let umur = 21;
+let tanggal = 23-11-2024;
+let menikah ="true";
+let belumMenikah="false";
+let beratBadan = 54.5;
+
+console.log(`Nama saya ${nama}, umur saya ${umur} tahun, berat badan saya ${beratBadan} dan saya Belum Menikah `);
